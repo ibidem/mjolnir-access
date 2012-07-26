a8737ca57615aa5b867964e9b9e16b6129466d20
srcspider <source.spider@gmail.com>
2012-07-26 18:32:36 +0300

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -2,14 +2,14 @@
 
 // This is a IDE honeypot. :)
 
-// HowTo: minion honeypot -n "ibidem\\access"
+// HowTo: order honeypot -n 'ibidem\access'
 
 class A12n extends \ibidem\access\A12n { /** @return \ibidem\access\A12n */ static function instance() { return parent::instance(); } }
 class Access extends \ibidem\access\Access {}
 class Controller_A12n extends \ibidem\access\Controller_A12n { /** @return \ibidem\access\Controller_A12n */ static function instance() { return parent::instance(); } }
 class Layer_Access extends \ibidem\access\Layer_Access { /** @return \ibidem\access\Layer_Access */ static function instance() { return parent::instance(); } }
 class Migration_User extends \ibidem\access\Migration_User { /** @return \ibidem\access\Migration_User */ static function instance() { return parent::instance(); } }
-class Model_DB_User extends \ibidem\access\Model_DB_User { /** @return \ibidem\access\Model_DB_User */ static function instance() { return parent::instance(); } }
+class Model_DB_User extends \ibidem\access\Model_DB_User {}
 class Model_HTTP_User extends \ibidem\access\Model_HTTP_User { /** @return \ibidem\access\Model_HTTP_User */ static function instance($id = null) { return parent::instance($id); } }
-class Model_User extends \ibidem\access\Model_User { /** @return \ibidem\access\Model_User */ static function instance() { return parent::instance(); } }
+class Model_User extends \ibidem\access\Model_User { /** @return \ibidem\access\Model_User */ static function instance($id = null) { return parent::instance($id); } }
 class Protocol extends \ibidem\access\Protocol { /** @return \ibidem\access\Protocol */ static function instance() { return parent::instance(); } }
