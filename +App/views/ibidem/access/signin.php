5379436bb17f8836f9f96a42d72cc066e9de79a0
srcspider <source.spider@gmail.com>
2012-09-05 17:01:18 +0300

    updated form syntax

--- +App/views/ibidem/access/signin.php
+++ +App/views/ibidem/access/signin.php
@@ -1,26 +1,26 @@
 <?
 	namespace app; 
-	
-	$access_relay = \app\CFS::config('ibidem/relays');
-	
+
 	$route_matcher = \app\URL::route('\ibidem\access\a12n');
 ?>
 
-<?= $form = \app\Form::instance()
-	->standard('twitter.general')
-	->action($route_matcher->url(array('action' => 'signin')))
+<?= $f = Form::i('twitter.general', $route_matcher->url(['action' => 'signin']))
 	->errors($errors['ibidem\a12n\signin'])
 	->classes(['marginless'])
 	->secure() ?>
 
 	<fieldset>
-		<?= $form->text(\app\Lang::msg('ibidem.access.signin.username_or_email'), 'identity')->attribute('autofocus', 'autofocus') ?>
-		<?= $form->password(\app\Lang::tr('Password'), 'password') ?>
-		<?= $form->select(null, 'remember_me', [ Lang::msg('ibidem.access.signin.remember_me') => 'on', Lang::msg('ibidem.access.signin.dont_remember_me') => 'off' ])
+		
+		<?= $f->text(Lang::msg('ibidem.access.signin.username_or_email'), 'identity')->attr('autofocus', 'autofocus') ?>
+		<?= $f->password(Lang::tr('Password'), 'password') ?>
+		<?= $f->select(null, 'remember_me', [ Lang::msg('ibidem.access.signin.remember_me') => 'on', Lang::msg('ibidem.access.signin.dont_remember_me') => 'off' ])
 			->value('off') ?>
+
 		<div class="form-actions">
-			<button class="btn btn-primary btn-large" tabindex="<?= \app\Form::tabindex() ?>"><i class="icon-signin"></i> <?= \app\Lang::tr('Sign In') ?></button>
+			<button class="btn btn-primary btn-large" <?= $f->sign() ?>>
+				<i class="icon-signin"></i> <?= Lang::tr('Sign In') ?>
+			</button>
 		</div>
 	</fieldset>
 
-<?= $form->close() ?>
+<?= $f->close() ?>
