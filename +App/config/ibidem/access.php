b50aa12d7228a22b60dd5181fb110f3a4ee89057
srcspider <source.spider@gmail.com>
2012-09-06 11:02:46 +0300

    tweaks

--- +App/config/ibidem/access.php
+++ +App/config/ibidem/access.php
@@ -1,56 +1,56 @@
 <?php namespace app; return array
 /////// Access Protocol Configuration //////////////////////////////////////////
 (
-		'whitelist' => array # allow
-			(
-				A12n::guest() => array
-					(
-						Allow::relays
-							(
-								'\ibidem\access\a12n',
-								'\ibidem\access\channel',
-								'\ibidem\access\endpoint'
-							)
-							->all_parameters(),
-					),
-			
-				// the following role acts as a template, include it in the
-				// alias list of your own administrator role
-				'+admin' => array
-					(
-						Allow::backend
-							(
-								'user-index', 
-								'user-edit',
-								'user-edit-profile',
-								'user-role-index',
-								'user-role-edit',
-								'user-profile-index',
-								'user-profile-edit',
-								'user-profile',
-								'user-access-settings'
-							)
-					),
-			),
-		'blacklist' => array # disallow! (no matter what)
-			(
-				// empty
-			),
-		'aliaslist' => array # alias list
-			(
-				/**
-				 * If something is allowed for the alias it will be allowed for 
-				 * the permission category as well. Does not apply for 
-				 * exceptions. If there is an exception for an alias the 
-				 * exception will not apply for the permission category.
-				 */
-			
-				// examples
-				# 'member' => [ \app\A12n::guest() ],
-				# 'admin'  => [ \app\A12n::guest(), 'member' ],
-			),
-		'roles' => array # roles in system
-			(
-				// empty
-			),
-	);
+	'whitelist' => array # allow
+		(
+			A12n::guest() => array
+				(
+					Allow::relays
+						(
+							'\ibidem\access\a12n',
+							'\ibidem\access\channel',
+							'\ibidem\access\endpoint'
+						)
+						->all_parameters(),
+				),
+
+			// the following role acts as a template, include it in the
+			// alias list of your own administrator role
+			'+admin' => array
+				(
+					Allow::backend
+						(
+							'user-index', 
+							'user-edit',
+							'user-edit-profile',
+							'user-role-index',
+							'user-role-edit',
+							'user-profile-index',
+							'user-profile-edit',
+							'user-profile',
+							'user-access-settings'
+						)
+				),
+		),
+	'blacklist' => array # disallow! (no matter what)
+		(
+			// empty
+		),
+	'aliaslist' => array # alias list
+		(
+			/**
+			 * If something is allowed for the alias it will be allowed for 
+			 * the permission category as well. Does not apply for 
+			 * exceptions. If there is an exception for an alias the 
+			 * exception will not apply for the permission category.
+			 */
+
+			// examples
+			# 'member' => [ \app\A12n::guest() ],
+			# 'admin'  => [ \app\A12n::guest(), 'member' ],
+		),
+	'roles' => array # roles in system
+		(
+			// empty
+		),
+);
