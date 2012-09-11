03db0d164bad237efa0a6fcc33a9ba7e138a4f94
srcspider <source.spider@gmail.com>
2012-09-11 16:49:11 +0300

    refactored ibidem to mjolnir

--- +App/views/mjolnir/access/auth.php
+++ +App/views/mjolnir/access/auth.php
@@ -8,7 +8,7 @@
 	<ul class="nav nav-pills nav-stacked">
 		<? foreach ($providers as $provider): ?>
 			<li>
-				<?= \app\View::instance()->file('ibidem/access/auth/'.$provider['slug'])->render() ?>
+				<?= \app\View::instance()->file('mjolnir/access/auth/'.$provider['slug'])->render() ?>
 			</li>
 		<? endforeach; ?>
 	</ul>
