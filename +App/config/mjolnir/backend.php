9ac3259e014bb291951639a23b1ddd7704e6e0db
srcspider <source.spider@gmail.com>
2012-09-12 11:22:30 +0300

    fix typo

--- +App/config/mjolnir/backend.php
+++ +App/config/mjolnir/backend.php
@@ -9,7 +9,7 @@
 						'icon' => 'reorder',
 						'title' => 'Profile Fields',
 						'context' => '\app\Backend_ProfileField',
-						'view' => 'ibidem/access/backend/profile-index'
+						'view' => 'mjolnir/access/backend/profile-index'
 					),
 			
 				'user-profile-edit' => array
@@ -17,7 +17,7 @@
 						'hidden' => true,
 						'title' => 'Edit Fields',
 						'context' => '\app\Backend_ProfileField',
-						'view' => 'ibidem/access/backend/profile-edit'
+						'view' => 'mjolnir/access/backend/profile-edit'
 					),
 			),
 	
@@ -28,49 +28,49 @@
 						'icon' => 'user',
 						'title' => 'User Manager',
 						'context' => '\app\Backend_User',
-						'view' => 'ibidem/access/backend/user-index'
+						'view' => 'mjolnir/access/backend/user-index'
 					),
 				'user-edit' => array
 					(
 						'hidden' => true,
 						'title' => 'Edit User',
 						'context' => '\app\Backend_User',
-						'view' => 'ibidem/access/backend/user-edit'
+						'view' => 'mjolnir/access/backend/user-edit'
 					),
 				'user-edit-profile' => array
 					(
 						'hidden' => true,
 						'title' => 'Edit User Profile',
 						'context' => '\app\Backend_User',
-						'view' => 'ibidem/access/backend/user-edit-profile'
+						'view' => 'mjolnir/access/backend/user-edit-profile'
 					),
 				'user-profile' => array
 					(
 						'hidden' => true,
 						'title' => 'View Profile',
 						'context' => '\app\Backend_User',
-						'view' => 'ibidem/access/backend/user-profile'
+						'view' => 'mjolnir/access/backend/user-profile'
 					),
 				'user-role-index' => array
 					(
 						'icon' => 'key',
 						'title' => 'User Roles',
 						'context' => '\app\Backend_Role',
-						'view' => 'ibidem/access/backend/role-index'
+						'view' => 'mjolnir/access/backend/role-index'
 					),
 				'user-role-edit' => array
 					(
 						'hidden' => true,
 						'title' => 'Edit Role',
 						'context' => '\app\Backend_Role',
-						'view' => 'ibidem/access/backend/role-edit'
+						'view' => 'mjolnir/access/backend/role-edit'
 					),
 				'user-access-settings' => array
 					(
 						'icon' => 'edit',
 						'title' => 'Settings',
 						'context' => '\app\Backend_Settings',
-						'view' => 'ibidem/access/backend/settings'
+						'view' => 'mjolnir/access/backend/settings'
 					),
 			),
 			
