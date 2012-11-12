7cd223e2eacde4977b1200fb65c0847f97986bde
Vlad Olaru <vlad.olaru@pixelgrade.com>
2012-11-12 09:07:27 +0200

    translated more from the password reset form

--- +App/config/lang/ro-ro/en-us.php
+++ +App/config/lang/ro-ro/en-us.php
@@ -7,4 +7,9 @@
 		'Cancel' => 'Anulează',
 		'Help' => 'Ajutor',
 		'Reset Password' => 'Resetează Parola',
+		'Failed to send the reset email to account address. Please try again later; if problem persists please contact us.' => 
+			'Din păcate nu am putut trimite emailul de reset către adresa de email a contului. Te rugăm să încerci mai târziu; dacă problema persistă contactează-ne.',
+		'We do not know of any such user or email.' => 'Nu am găsit un astel de username sau adresă de email',
+		'You\'ve failed the <a href="http://en.wikipedia.org/wiki/CAPTCHA">CAPTCHA</a> check.'
+			=> 'Nu ai trecut verificarea <a href="http://en.wikipedia.org/wiki/CAPTCHA">CAPTCHA</a>.',
 	);
