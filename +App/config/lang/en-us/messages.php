9f9b101f25852586280e28df65d5c855b9642282
srcspider <source.spider@gmail.com>
2012-12-03 16:42:29 +0200

    Signin changed to Sign In

--- +App/config/lang/en-us/messages.php
+++ +App/config/lang/en-us/messages.php
@@ -3,7 +3,7 @@
 		'mjolnir.access.sign_up_now' => 'Sign Up now!',
 
 		'mjolnir.access.signin.username_or_email' => '<b>Username</b> or <b>Email</b>',
-		'mjolnir.access.signin.title' => 'Signin',
+		'mjolnir.access.signin.title' => 'Sign In',
 		'mjolnir.access.signin.remember_me' => 'Private area. Remember Me...',
 		'mjolnir.access.signin.not_yet_a_member' => 'Not yet a member?',
 		'mjolnir.access.signin.dont_remember_me' => 'Public area; temporary login.',
