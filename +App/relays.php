b50aeeecfca2fd31acfe3f2435aaffd8f7a654e4
srcspider <source.spider@gmail.com>
2013-01-17 11:57:50 +0200

    refactored Relay into Router

--- +App/relays.php
+++ +App/relays.php
@@ -2,6 +2,6 @@
 
 $mvc = \app\CFS::config('mjolnir/layer-stacks')['mvc'];
 
-\app\Relay::process('\mjolnir\access\channel', $mvc);
-\app\Relay::process('\mjolnir\access\endpoint', $mvc);
-\app\Relay::process('\mjolnir\access\a12n', $mvc);
+\app\Router::process('\mjolnir\access\channel', $mvc);
+\app\Router::process('\mjolnir\access\endpoint', $mvc);
+\app\Router::process('\mjolnir\access\a12n', $mvc);
