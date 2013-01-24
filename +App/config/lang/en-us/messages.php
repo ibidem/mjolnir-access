80d560d291612b7b45a9d1f0a7647ecbbe4a0670
srcspider <source.spider@gmail.com>
2013-01-24 14:48:29 +0200

    missing message

--- +App/config/lang/en-us/messages.php
+++ +App/config/lang/en-us/messages.php
@@ -67,7 +67,11 @@ this account.',
 			},
 					
 		'mjolnir:sent_activation_email'
-			=> 'Success! But your account is currently inactive. An email has been sent to your email address with activation instructions. Attempting to signin will re-issue a new activation email.'
-					
+			=> 'Success! But your account is currently inactive. An email has been sent to your email address with activation instructions. Attempting to signin will re-issue a new activation email.',
+				
+		'login.passwordattemps' => function ($in)
+			{
+				return \strtr('You\'ve entered the wrong password :number times.', $in);
+			},
 	);
 	
