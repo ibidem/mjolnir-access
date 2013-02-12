f0235f3626d226c08172abd4bb61b4720c42a480
srcspider <source.spider@gmail.com>
2013-02-12 18:08:36 +0200

    refactored stack from mvc to public

--- +App/relays.php
+++ +App/relays.php
@@ -1,6 +1,6 @@
 <?php namespace mjolnir\access;
 
-$mvc = \app\CFS::config('mjolnir/layer-stacks')['mvc'];
+$mvc = \app\CFS::config('mjolnir/layer-stacks')['public'];
 
 \app\Router::process('\mjolnir\access\channel', $mvc);
 \app\Router::process('\mjolnir\access\endpoint', $mvc);
