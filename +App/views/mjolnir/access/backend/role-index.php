e617e0aaa856438852244e5e5eeb06650d19807c
srcspider <source.spider@gmail.com>
2013-02-26 00:36:03 +0200

    refactored to use backend/template

--- +App/views/mjolnir/access/backend/role-index.php
+++ +App/views/mjolnir/access/backend/role-index.php
@@ -1,79 +1,33 @@
 <?
 	namespace app;
 
-	/* @var $context Backend_Role */
+	/* @var $context Backend_User */
 	/* @var $control Controller_Backend */
 
-	$page = isset($_GET['page']) ? $_GET['page'] : 1;
-	$pagelimit = 10;
+	$columns = array
+		(
+			'title' => 'Role',
+		);
+
+	$actions = array
+		(
+			'user-role-edit' => array
+				(
+					'title' => 'Edit',
+					'class' => 'btn-warning',
+				)
+		);
 ?>
 
-<h1>Users</h1>
+<h1>Roles</h1>
 
-<? $roles = $context->entries($page, $pagelimit) ?>
-
-<? if ( ! empty($roles)): ?>
-
-	<?= $form = HTML::form($control->action('delete'))
-		->addfieldtemplate(':field') ?>
-
-	<table class="table table-striped marginless">
-		<thead>
-			<tr>
-				<th class="micro-col">&nbsp;</th>
-				<th>role</th>
-				<th>&nbsp;</th>
-			</tr>
-		</thead>
-		<tbody>
-			<? foreach ($roles as $role): ?>
-				<tr>
-					<td>
-						<?= $form->checkbox(null, 'selected[]')
-							->value_is($role['id']) ?>
-					</td>
-					<td><?= $role['title'] ?></td>
-					<td class="table-controls">
-
-						<a href="<?= $control->backend('user-role-edit') ?>?id=<?= $role['id'] ?>"
-						   class="btn btn-mini btn-warning">
-							Edit
-						</a>
-
-						<?= $delete_form = HTML::form($control->action('erase'), 'mjolnir:inline') ?>
-						<?= $delete_form->hidden('id')->value_is($role['id']) ?>
-						<button class="btn btn-mini btn-danger" <?= $delete_form->mark() ?>>
-							Delete
-						</button>
-
-					</td>
-				</tr>
-			<? endforeach; ?>
-		</tbody>
-	</table>
-
-	<div class="row-fluid">
-
-		<br/>
-		<div class="pull-right marginless-pagination">
-			<?= $context->pager()
-				->pagelimit_is($pagelimit)
-				->page_is($page)
-				->apply('twitter') ?>
-		</div>
-		
-		<button class="btn btn-danger btn-mini" <?= $form->mark() ?>>
-			<i class="icon-trash"></i> Delete Selected
-		</button>
-
-	</div>
-
-<? else: # no users in system ?>
-	<br/>
-	<p class="alert alert-info">There are currently <strong>no roles</strong> defined.</p>
-<? endif; ?>
-
-<hr/>
+<?= \app\View::instance('mjolnir/backend/template/indexer')
+	->inherit($this) # inject context, control, etc
+	->pass('columns', $columns)
+	->pass('actions', $actions)
+	->pass('plural', 'roles')
+	->pass('singular', 'role')
+	->render() ?>
 
 <div role="application">
 	<h2>New Role</h2>
