5bd5deba89b0b30d6d9ba8808ed3b22c342c3d9f
srcspider <source.spider@gmail.com>
2013-03-05 11:26:14 +0200

    removed signout

--- Trait/Controller/MjolnirSignin.php
+++ Trait/Controller/MjolnirSignin.php
@@ -30,11 +30,6 @@ trait Trait_Controller_MjolnirSignin
 		// this method is not guranteed since some implementations allow for 
 		// login from all user roles
 		$this->redirect_signedin_users();
-		
-		// gurantee current user is signed out before attempting to login new
-		// user; this is a problem on pages with login pages that do not take
-		// account if a user is signed in or not and just display the form
-		\app\Auth::signout();
 
 		if (\app\Server::request_method() === 'POST')
 		{
